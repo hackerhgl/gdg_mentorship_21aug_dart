3cc6b58ab236f8c5f464cdc5a6142fb3df373e4f
hackerhgl <hackerhgl@gmail.com>
2021-08-21 16:18:08 +0500

    Arrays and loop

---
+++ bin/arrays.dart
@@ -0,0 +1,20 @@
+void main(List<String> args) {
+  final List<dynamic> list = ["a", "b", "c", 1, 2, "last"];
+
+  final length = list.length;
+
+  // print(length);
+  // print(list[length - 1]);
+
+  // for (var i = 0; i < length; i++) {
+  //   print(list[i]);
+  // }
+
+  // for (var value in list) {
+  //   print(value);
+  // }
+
+  // list.forEach((element) {
+  //   print(element);
+  // });
+}
